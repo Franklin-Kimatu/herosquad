673503c73626b1e205c0308bf172a7de0a1325df
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-07 23:29:30 +0300

    adding a route for deleting all heros

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,14 +3,28 @@ import models.Hero;
 import org.sql2o.Sql2o;
 import spark.Spark;
 import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        String connectionString = "jdbc:h2:~/todolist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        String connectionString = "jdbc:h2:~/herosquad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
-        Sql2oHeroDao taskDao = new Sql2oHeroDao(sql2o);
-        get("/hello", (request, response) -> "Hello Friend!");
+        Sql2oHeroDao heroDao = new Sql2oHeroDao(sql2o);
+
+        //get : delete all heros
+        get ("/heros/delete",(request, response) -> {
+            Map<String,Object> model= new HashMap<>();
+            heroDao.clearAllHeros();
+            response.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        //get:
     }
 }
