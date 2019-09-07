41bb59918aea8fe5915fa15e7fb7aed8eef07937
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-07 12:55:48 +0300

    adding the hero and the tests

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,49 @@
 package models;
 
+import java.util.Objects;
+
 public class Hero {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return age == hero.age &&
+                Objects.equals(name, hero.name) &&
+                Objects.equals(power, hero.power) &&
+                Objects.equals(weakness, hero.weakness);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, power, weakness);
+    }
+
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+
+
+    public Hero(String name,int age,String power, String weakness){
+
+        this.name=name;
+        this.age= age;
+        this.power=power;
+        this.weakness=weakness;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public String getPower(){
+        return power;
+    };
+    public String getWeakness(){
+        return weakness;
+    }
+
 }
---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,45 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+
+    @Test
+    public void newHeroInstantiatesCorrectly_true(){
+        Hero hero= setUpNewHero();
+        assertEquals(true,hero instanceof Hero);
+    }
+
+    @Test
+    public void heroMustHaveAName(){
+        Hero hero =setUpNewHero();
+        assertEquals("superman",hero.getName());
+    }
+    //test age
+    @Test
+    public void getAgeInstantiatesTheHeroAge(){
+        Hero hero = setUpNewHero();
+        assertEquals(100,hero.getAge());
+    }
+
+    //tests power
+    @Test
+    public void getPowerChecksTheHerosPower(){
+        Hero hero =setUpNewHero();
+        assertEquals("height",hero.getPower());
+    }
+
+    //tests weakness
+    @Test
+    public void getWeaknessInstantiatesHeroWeakness(){
+        Hero hero =setUpNewHero();
+        assertEquals("light",hero.getWeakness());
+    }
+    private Hero setUpNewHero() {
+        return new Hero("superman",100,"height","light");
+    }
+
+
+}
