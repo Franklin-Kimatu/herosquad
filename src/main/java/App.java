e7f7aa8a892d5868c618644e42d0a12ed0fc159e
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-08 11:42:00 +0300

    adding functunality to the nav bar

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,22 +17,27 @@ public class App {
         Sql2o sql2o = new Sql2o(connectionString, "", "");
         Sql2oHeroDao heroDao = new Sql2oHeroDao(sql2o);
 
-        //get : delete all heros
-        get ("/heros/delete",(request, response) -> {
-            Map<String,Object> model= new HashMap<>();
-            heroDao.clearAllHeros();
-            response.redirect("/");
-            return null;
+        //get to show new hero form
+        get("/heros/new",(request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model,"hero-form.hbs");
+
+        },new HandlebarsTemplateEngine());
+
+
+        //task: process new hero form
+        post ("/heros/new",(request, response) -> {
+            Map<String,Object>model = new HashMap<>();
+            String name =request.queryParams("name");
+
+            String power= request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            Hero newIdentity =new Hero(name,power,weakness);
+            heroDao.add(newIdentity);
+
+            return new ModelAndView(model,"success.hbs");
         }, new HandlebarsTemplateEngine());
 
-        //get: delete an individual hero
-        get("/heros/:id/delete" ,(request, response) -> {
-            Map<String,Object>model = new HashMap<>();
-            int idOfHeroToDelete = Integer.parseInt(request.params("id"));
-            heroDao.deleteById(idOfHeroToDelete);
-            response.redirect("/");
-            return null;
-        },new HandlebarsTemplateEngine());
 
         //get to show all heros
         get("/",(request, response) -> {
@@ -43,27 +48,7 @@ public class App {
 
         }, new HandlebarsTemplateEngine());
 
-        //get to show new hero form
-        get("/heros/new",(request, response) -> {
-            Map<String,Object> model = new HashMap<>();
-            return new ModelAndView(model,"hero-form.hbs");
-
-        },new HandlebarsTemplateEngine());
-
-        //task: process new hero form
-        post ("/heros",(request, response) -> {
-            Map<String,Object>model = new HashMap<>();
-            String name =request.queryParams("name");
-//
-            String power= request.queryParams("power");
-            String weakness = request.queryParams("weakness");
-            Hero newIdentity =new Hero(name,power,weakness);
-            heroDao.add(newIdentity);
-            response.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
-
-        //show an individual form
+        //show an individual hero
         get("/heros/:id",(request, response) -> {
             Map<String,Object>model = new HashMap<>();
             int idOfHeroToFind = Integer.parseInt(request.params("id"));
@@ -83,15 +68,42 @@ public class App {
         },new HandlebarsTemplateEngine());
 
         //task:process a form to update a hero
-        post("heros/:id",(request, response) -> {
+        post("heros/:id/update",(request, response) -> {
             Map<String,Object>model = new HashMap<>();
             String newName = request.queryParams("name");
             String newPower = request.queryParams("power");
             String newWeakness = request.queryParams("weakness");
             int idOfTheHeroToEdit =Integer.parseInt(request.params("id"));
             heroDao.update(idOfTheHeroToEdit,newName,newPower,newWeakness);
+
+            return new ModelAndView(model,"success.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //get : delete all heros
+        get ("/heros/delete",(request, response) -> {
+            Map<String,Object> model= new HashMap<>();
+            heroDao.clearAllHeros();
+            response.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete an individual hero
+        get("/heros/:id/delete" ,(request, response) -> {
+            Map<String,Object>model = new HashMap<>();
+            int idOfHeroToDelete = Integer.parseInt(request.params("id"));
+            heroDao.deleteById(idOfHeroToDelete);
             response.redirect("/");
             return null;
         },new HandlebarsTemplateEngine());
+
+
+
+
+
+
+
+
+
+
     }
 }
